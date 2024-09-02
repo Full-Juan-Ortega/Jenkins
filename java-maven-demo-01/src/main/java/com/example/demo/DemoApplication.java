dee703d5b13aa39ac077930043422a9b34896bab
Juan Ortega <juan.ortega.it@gmail.com>
2024-09-01 21:03:03 -0300

    pepe

--- java-maven-demo-01/src/main/java/com/example/demo/DemoApplication.java
+++ java-maven-demo-01/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("hola mundo");
 	}
 
+
+
 }
+
--- java-maven-demo-01/src/main/java/com/example/demo/random.java
+++ java-maven-demo-01/src/main/java/com/example/demo/random.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class random {
+
+    void cualquiera (){
+        System.out.println("esto es cualquiera");
+    }
 
 }
